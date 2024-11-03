1ac02b5bba9b14ec9b7b1ba037f21c935993e2bf
GianiStatie <gianistatie@gmail.com>
2024-11-03 18:07:20 +0000

    working db on server

--- db/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash 
-
-mariadb -uroot -pAVeryComplicatedPassword
-CREATE USER 'leaderboard'@'localhost' IDENTIFIED BY 'AVeryComplicatedPassword';
-GRANT ALL PRIVILEGES ON leaderboard.* TO leaderboard@'localhost';
-FLUSH PRIVILEGES;
-exit
